045cebf3f60003e48908d714d624d7232ccc41b2
huangxionging <huangxionging@163.com>
2015-09-16 09:12:21 +0800

    更新
    更新

--- HXPieChart/HXPieChartView.m
+++ HXPieChart/HXPieChartView.m
@@ -11,27 +11,47 @@
 @implementation HXPieChartView
 
 
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        _dataArray = [[NSMutableArray alloc] init];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(self.width / 2 - 50, self.height / 2 - 6, 100, 12)];
+        label.text = @"折扣数据";
+        label.adjustsFontSizeToFitWidth = YES;
+        
+        [self addSubview: label];
+    }
+    
+    return self;
+}
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
     
     CGContextRef ctx = UIGraphicsGetCurrentContext();
-    CGContextSetLineWidth(ctx, 50);
+    CGContextSetLineWidth(ctx, 55);
     
-    for (NSInteger index = 0; index < 1; ++index) {
+    CGFloat angle = 1;
+    for (NSInteger index = 0; index < _dataArray.count; ++index) {
         
         HXPieModel *model = _dataArray[index];
         
-
+        CGContextSetStrokeColorWithColor(ctx, UIColorFromRGB(model.colorValue).CGColor);
         
+        CGContextAddArc(ctx, self.centerX, self.bottom - self.centerY, 90, angle, angle + M_PI * 2 * model.value, NO);
+        
+        CGContextDrawPath(ctx, kCGPathStroke);
+        angle += M_PI * 2 * model.value;
         
     }
 }
 
-- (CGFloat) getAngleWith: (CGFloat) value {
-    return M_PI * 2 * value / 100;
-}
+//- (CGFloat) getAngleWith: (CGFloat) value {
+//    return ;
+//}
 
 
 @end
